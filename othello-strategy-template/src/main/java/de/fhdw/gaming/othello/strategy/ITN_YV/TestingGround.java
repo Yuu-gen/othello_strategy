d1cbb2a1214870a3a876dc5fcafd25de29f17a7c
outeh <outeh@outeh-pc>
2020-09-02 19:58:57 +0200

    for loop version of testing ground

--- othello-strategy-template/src/main/java/de/fhdw/gaming/othello/strategy/ITN_YV/TestingGround.java
+++ othello-strategy-template/src/main/java/de/fhdw/gaming/othello/strategy/ITN_YV/TestingGround.java
@@ -1,35 +1,24 @@
 package de.fhdw.gaming.othello.strategy.ITN_YV;
 
 import java.util.List;
-import java.util.ListIterator;
 
 public class TestingGround {
 
     public static void main(final String[] args) {
         final Node<String> root = testgen();
         final List<List<Node<String>>> lNodes = root.getOrganizedLowestLayer();
-        final ListIterator i = lNodes.listIterator();
-        while (i.hasNext()) {
-            final List<Node<String>> currentList = (List<Node<String>>) i.next();
-            final ListIterator j = currentList.listIterator();
-            while (j.hasNext()) {
-                final Node<String> currentNode = (Node<String>) j.next();
-                currentNode.deleteNode();
+        for (int i = 0; i < lNodes.size(); i++) {
+            for (int j = 0; j < lNodes.get(i).size(); j++) {
+                lNodes.get(i).get(j).deleteNode();
             }
         }
-
-//        for (int i = 0; i < lNodes.size(); i++) {
-//            for (int j = 0; j < lNodes.get(i).size(); j++) {
-//                lNodes.get(i).get(j).deleteNode();
-//            }
-//        }
 //        for (int i = 0; i < lNodes.size(); i++) {
 //            for (int j = 0; j < lNodes.get(i).size(); j++) {
 //                lNodes.get(i).get(j).deleteNode();
 //            }
 //        }
         System.out.println(root);
-        // System.out.println(testgen().toString());
+        // System.out.println(testgen().toString());d
 
     }
 
